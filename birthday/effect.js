9f21a7545b8e0c8bb9a8b5fa6bcc65b4e96fe8fa
unknown <hsiao_kl@water.gov.tw>
2017-12-29 15:47:09 +0800

    test

--- birthday/effect.js
+++ birthday/effect.js
@@ -187,8 +187,8 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==63){
-				$("p:nth-child(62)").fadeOut('slow').promise().done(function () {
+			if(i==65){
+				$("p:nth-child(64)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
 				
